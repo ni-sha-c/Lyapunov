95d17d4e914486a812abc2fd6e3f0f1ac063fa8f
Nisha Chandramoorthy <nishac92@gmail.com>
2019-09-03 18:48:04 -0400

    Fix errors in CLV class methods. Write basic unit test for LEs.

--- clv.py
+++ clv.py
@@ -4,7 +4,7 @@ sys.path.insert(0,'examples/')
 from lorenz63 import Runner
 
 class CLV():
-    def init(self):
+    def __init__(self):
         self.runner = Runner()
         self.subspace_dim = 3
 
@@ -13,7 +13,7 @@ class CLV():
         parameter = 0.
         runner = self.runner
         d = runner.state_dim
-        stateInit = rand(d)
+        stateInit = random.rand(d)
         stateInit, objectiveTrj = runner.primalSolver(stateInit,\
             parameter, nSpinUp)
         d_u = self.subspace_dim
@@ -21,8 +21,9 @@ class CLV():
         self.nSteps_forward = self.nSteps//2
         self.nSteps_backward = self.nSteps//2
         nSteps_backward = self.nSteps_backward
-        tangents_mt1 = rand(d_u, d)
-        tangents_mt1, R = np.linalg.qr(tangents_mt1.T)
+        tangents_mt1 = random.rand(d_u, d)
+        tangents_mt1, R = linalg.qr(tangents_mt1.T)
+        self.R = R
         self.tangents = tangents_mt1.T
         self.lyap_exps = zeros(d_u)
         self.primal = stateInit
@@ -41,13 +42,15 @@ class CLV():
         runner = self.runner
         tangents = self.tangents
         lyap_exps = self.lyap_exps
-
+        R = self.R
+        d_u = self.subspace_dim
+        parameter = 0.
         for i in range(nSteps_forward):
             for j in range(d_u):
                 tangents[j], sensitivity = runner.tangentSolver(\
                     tangents[j], primal, parameter, 1,\
                     homogeneous=True)
-            tangents, R = np.linalg.qr(tangents.T)
+            tangents, R = linalg.qr(tangents.T)
             lyap_exps += log(abs(diag(R)))/nSteps/runner.dt
             tangents = tangents.T
             primal, objectiveTrj = runner.primalSolver(primal, \
@@ -63,6 +66,10 @@ class CLV():
         runner = self.runner
         tangents = self.tangents
         primal = self.primal
+        R = self.R
+        parameter = 0.
+        d_u = self.subspace_dim
+        nSteps = self.nSteps
         for i in range(self.nSteps_backward):
             RTrj[i] = R
             QTrj[i] = tangents
@@ -70,7 +77,7 @@ class CLV():
                 tangents[j], sensitivity = runner.tangentSolver(\
                     tangents[j], primal, parameter, 1,\
                     homogeneous=True)
-            tangents, R = np.linalg.qr(tangents.T)
+            tangents, R = linalg.qr(tangents.T)
             lyap_exps += log(abs(diag(R)))/nSteps/runner.dt
             tangents = tangents.T
             primal, objectiveTrj = runner.primalSolver(primal, \
@@ -78,14 +85,15 @@ class CLV():
 
     def backward_steps(self):
         self.forward_steps()
+        d_u = self.subspace_dim
         self.coeffsTrj[-1] = eye(d_u,d_u)
         self.clvs[-1] = self.QTrj[-1]
         for i in range(self.nSteps_backward-1,0,-1):
             self.coeffsTrj[i-1] = linalg.solve(self.RTrj[i], \
                     self.coeffsTrj[i]) 
-            self.coeffsTrj[i-1] /= norm(self.coeffsTrj[i-1],axis=0)
+            self.coeffsTrj[i-1] /= linalg.norm(self.coeffsTrj[i-1],axis=0)
             self.clvs[i-1] = dot(self.QTrj[i-1], self.coeffsTrj[i-1])
-            self.clvs[i-1] /= norm(self.clvs[i-1],axis=0)
+            self.clvs[i-1] /= linalg.norm(self.clvs[i-1],axis=0)
 
     def compute_les_and_clvs(self):
         self.backward_steps()
